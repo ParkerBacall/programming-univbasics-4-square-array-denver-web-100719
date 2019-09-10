93abd3e1d2e72ad6cbfd9ca8aa5c7be28e8c3c18
Parker Bacall <pbacall2@gmail.com>
2019-09-10 04:04:44 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,10 +2,8 @@ def square_array(array)
   count = 0
 
   while array[count] do
-    array[count] = array[count] ** 2
-    p array[count]
+    p array[count] ** 2
 
     count = count + 1
-
   end
 end
